f8c2ad1be2d2f647351d6e6b0d8fed4e7a81c45f
Jeremey Schrack <jdschrack@gmail.com>
2023-07-08 12:03:33 -0400

    Starting Dotnet 7 unit tests

---
+++ src/FaxCore.Ev6.Test.Net7/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FaxCore.Ev6.Test.Net7;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
